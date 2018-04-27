4a7132ca4d2db05c8d20908aa2f6da60d1f47ce8
Sebastian Plamauer <oeplse@gmail.com>
2018-04-27 13:20:11 +0200

    More experiments

--- src/main_nucleo_f412zg.kt
+++ src/main_nucleo_f412zg.kt
@@ -17,36 +17,46 @@
 import platform.zephyr.nucleo_f412zg.*
 import kotlinx.cinterop.*
 
-class Pin(port: String, pin: Int) {
-    var state: Boolean = false
-    val port: String = port
-    val pin: Int = pin
-    val dev = device_get_binding(port)
+fun Boolean.toInt() = if (this) 1 else 0
+
+class Pin(port: String, pin: Int, dir: Int) {
+    private val port: String = port
+    private val pin: Int = pin
+    private val dir: Int = dir
+    private val dev = device_get_binding(port)
 
     init {
-        gpio_pin_configure(this.dev, this.pin, GPIO_DIR_OUT)
-        gpio_pin_write(this.dev, this.pin, 0)
-        this.state = false
+        gpio_pin_configure(this.dev, this.pin, this.dir)
     }
 
-    fun on() {
+    fun write(value: Int) {
+        gpio_pin_write(this.dev, this.pin, value)
+    }
+
+    fun high() {
         gpio_pin_write(this.dev, this.pin, 1)
-        this.state = true
     }
 
-    fun off() {
+    fun low() {
         gpio_pin_write(this.dev, this.pin, 0)
-        this.state = false
+    }
+
+    fun read(): Int {
+        var value: Int = 0
+        gpio_pin_read(this.dev, this.pin, cValuesOf(value))
+        return value
     }
 }
 
 fun main(args: Array<String>) {
-    val led = Pin(LED0_GPIO_PORT, LED0_GPIO_PIN)
+    val led = Pin(LED0_GPIO_PORT, LED0_GPIO_PIN, GPIO_DIR_OUT)
+    val sw = Pin(USER_PB_GPIO_PORT, USER_PB_GPIO_PIN, GPIO_DIR_IN)
 
     while (true) {
-        led.on()
-        k_sleep(100);
-        led.off()
-        k_sleep(100);
+        led.write(1)
+        k_sleep(1000)
+        led.write(0)
+        k_sleep(1000)
+        printk("Hello World! %s\n", CONFIG_ARCH);
     }
 }
